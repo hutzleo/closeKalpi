e87a35fd0265a3de4d664d7b19bc6a94a65f6f30
Oded Hutzler <odedhutzler@odeds-air.home>
2017-06-14 19:25:04 +0300

    add files

---
+++ locationsList.js
@@ -0,0 +1,762 @@
+var locations = [  
+   {  
+      "city":"קרייתשמונה",
+      "adress":"כיכר ההסתדרות 1 קריית שמונה - מועדון הגימלאים",
+      "hours":"11: 00-21: 00",
+      "location":{  
+         "lat":33.2052604,
+         "lng":35.5685358
+      },
+      "data":"Kikar Hahistadrut 1, Kiryat Shmona, Israel"
+   },
+   {  
+      "city":"טבריה",
+      "adress":"אורחיים1טבריה-מתנ\"ס פירסט",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.778846,
+         "lng":35.5189105
+      },
+      "data":"Or Haim St 1, Tiberias, Israel"
+   },
+   {  
+      "city":"יקנעם עילית",
+      "adress":"האלונים 35 יוקנעם עילית - מרכז הטניס",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.6472064,
+         "lng":35.09380549999999
+      },
+      "data":"Ha-Alonim St 35, Yokne'am Illit, Israel"
+   },
+   {  
+      "city":"מגדל העמק",
+      "adress":"קדש 34 מגדל העמק- בית החברה",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":32.6779055,
+         "lng":35.2388673
+      },
+      "data":"Kadesh St 34, Migdal HaEmek, Israel"
+   },
+   {  
+      "city":"משמר העמק",
+      "adress":"משמר העמק - מועדון לחבר ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":32.6117295,
+         "lng":35.1412005
+      },
+      "data":"Mishmar HaEmek, Israel"
+   },
+   {  
+      "city":"נצרת עילית",
+      "adress":"צבעוני 14 נצרת עלית",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.6967278,
+         "lng":35.31101150000001
+      },
+      "data":"Tsiv'oni St 14, Nazareth Iliit, Israel"
+   },
+   {  
+      "city":"עין חרוד",
+      "adress":"עין חרוד מאוחד חדר האוכל ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":32.556806,
+         "lng":35.392995
+      },
+      "data":"Ein Harod, Israel"
+   },
+   {  
+      "city":"עפולה",
+      "adress":"עומר 5 עפולה ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":32.6124426,
+         "lng":35.2971198
+      },
+      "data":"Omer St 5, Afula, Israel"
+   },
+   {  
+      "city":"קריית טבעון",
+      "adress":"אלונים 85 קריית טבעון - עמותת שלהבת",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.7217724,
+         "lng":35.1298275
+      },
+      "data":"Alonim St 85, Kiryat Tiv'on, Israel"
+   },
+   {  
+      "city":"בית שאן",
+      "adress":"שדרות מנחם בגין 129 בית שאן - אכסניית אנא ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":32.4963251,
+         "lng":35.5052162
+      },
+      "data":"Menachem Begin Blvd 129, Beit She'an, Israel"
+   },
+   {  
+      "city":"כרמיאל",
+      "adress":"משגב 17 כרמיאל - סניף המפלגה",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.9189514,
+         "lng":35.2959878
+      },
+      "data":"Misgav St 17, Karmiel, Israel"
+   },
+   {  
+      "city":"מעלות-תרשיחא",
+      "adress":"הרב קוק 87 מעלות תרשיחא ליד משרדי נתיב אקספרס",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":33.0147666,
+         "lng":35.28111
+      },
+      "data":"HaRav Kuk St 87, Ma'alot-Tarshiha, Israel"
+   },
+   {  
+      "city":"נהריה",
+      "adress":"שדרות הגעתון 31 , נהריה - אולמי פינגווין",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":33.0065148,
+         "lng":35.0936822
+      },
+      "data":"Sderot HaGa'aton 31, Nahariya, 22440, Israel"
+   },
+   {  
+      "city":"עכו",
+      "adress":"הרב הרצוג 16 עכו - מתנ\"ס היימן ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":32.9327067,
+         "lng":35.0756021
+      },
+      "data":"Ha-Rav Herzog St 16, Acre, Israel"
+   },
+   {  
+      "city":"קריית מוצקין",
+      "adress":"שדרות בן גוריון 82 קריית מוצקין - מרכז לב הקריות",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.8397211,
+         "lng":35.0808015
+      },
+      "data":"Sderot Ben Gurion 82, Kiryat Motskin, Israel"
+   },
+   {  
+      "city":"חיפה",
+      "adress":"דרך פיקא 69 חיפה - היכל הספורט רוממה קפה קפה",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.7878124,
+         "lng":34.9970818
+      },
+      "data":"היכל הספורט רוממה, דרך פיק\"א 69, חיפה, Israel"
+   },
+   {  
+      "city":"יגור",
+      "adress":"קיבוץ יגור - מועדון לחבר ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":32.7403455,
+         "lng":35.0783116
+      },
+      "data":"Yagur, Israel"
+   },
+   {  
+      "city":"רמת יוחנן",
+      "adress":"רמת יוחנן בית מוסדות הספרייה",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":32.793457,
+         "lng":35.122208
+      },
+      "data":"Ramat Yohanan, Israel"
+   },
+   {  
+      "city":"זכרון יעקב",
+      "adress":"מרבד הקסמים 16 זכרון יעקב - אשכול פיס ג\"מבורי ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":32.5620193,
+         "lng":34.9635988
+      },
+      "data":"Marvad ha-Ksamim St 16, Zikhron Ya'akov, Israel"
+   },
+   {  
+      "city":"פרדס חנה-כרכור",
+      "adress":"דרך הבנים פרדס חנה כרכור - בית יד לבנים",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.4761203,
+         "lng":34.9845645
+      },
+      "data":"Derech HaBanim, Pardes Hana-Karkur, Israel"
+   },
+   {  
+      "city":"חדרה",
+      "adress":"הרב הרצוג 8 חדרה - בית ויצ\"ו",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.4390938,
+         "lng":34.9130977
+      },
+      "data":"HaRav Herzog St 8, Hadera, Israel"
+   },
+   {  
+      "city":"שדות ים",
+      "adress":"שדות ים - מועדון החברים מתחת לחדר האוכל ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":32.492613,
+         "lng":34.89315
+      },
+      "data":"Sdot Yam, Israel"
+   },
+   {  
+      "city":"מעגן מיכאל",
+      "adress":"מעגן מיכאל - מועדון האסם ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":32.5563622,
+         "lng":34.9174571
+      },
+      "data":"Ma'agan Michael, Israel"
+   },
+   {  
+      "city":"נתניה",
+      "adress":"מקדונלד 24 נתניה - מרכז מורשת טוניס",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.3343103,
+         "lng":34.8534662
+      },
+      "data":"MacDonald St 24, Netanya, Israel"
+   },
+   {  
+      "city":"אבן יהודה",
+      "adress":"המייסדים 53 אבן יהודה - בית וולדמן",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.2696179,
+         "lng":34.8881597
+      },
+      "data":"Ha-Meyasdim St 53, Even Yehuda, Israel"
+   },
+   {  
+      "city":"הוד השרון",
+      "adress":"דרך מגדיאל 67 הוד השרון - בית האיכר",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.1602771,
+         "lng":34.9023055
+      },
+      "data":"Derech Magdiel 67, Hod HaSharon, Israel"
+   },
+   {  
+      "city":"כפר סבא",
+      "adress":"התע\"ש 14 כפר סבא - מרכז חאן",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.1743746,
+         "lng":34.9271584
+      },
+      "data":"HaTa'as St 14, Kefar Sava, Israel"
+   },
+   {  
+      "city":"רעננה",
+      "adress":"קלאוזנר 3 רעננה - בית ויצו",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.1798509,
+         "lng":34.8756891
+      },
+      "data":"Klausner St 3, Ra'anana, Israel"
+   },
+   {  
+      "city":"הרצליה",
+      "adress":"הבנים 11 הרצליה - מועדון פועלי בניין",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.162796,
+         "lng":34.8439786
+      },
+      "data":"HaBanim St 11, Herzliya, Israel"
+   },
+   {  
+      "city":"רמת השרון",
+      "adress":"המסגר 3 רמת השרון - צצה אירועים",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.1374822,
+         "lng":34.8439004
+      },
+      "data":"HaMasger St 3, Ramat Hasharon, Israel"
+   },
+   {  
+      "city":"ירושלים",
+      "adress":"אברהם מאפו 1 , ירושלים - בית ויצ\"ו",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.7731898,
+         "lng":35.2223687
+      },
+      "data":"Avraham Mapu St 1, Jerusalem, Israel"
+   },
+   {  
+      "city":"מבשרת ציון",
+      "adress":"רקפת 1 מבשרת ציון - מתנס",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.8014614,
+         "lng":35.1582195
+      },
+      "data":"Rakefet St 1, Mevaseret Zion, Israel"
+   },
+   {  
+      "city":"בית שמש",
+      "adress":"הנשיא 40 בית שמש - קיבוץ עירוני תמוז",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.7481806,
+         "lng":34.9799664
+      },
+      "data":"HaNasi St 40, Bet Shemesh, Israel"
+   },
+   {  
+      "city":"תל אביב",
+      "adress":"אושן - שדרות רוקח 101 מרכז הירידים",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.1053445,
+         "lng":34.8110548
+      },
+      "data":"Rokach Blvd 101, Tel Aviv-Yafo, Israel"
+   },
+   {  
+      "city":"תל אביב",
+      "adress":"שדרות הבעש\"ט 35 תל אביב יפו",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.0382251,
+         "lng":34.7550369
+      },
+      "data":"Sderot HaBesht 35, Tel Aviv-Yafo, Israel"
+   },
+   {  
+      "city":"מודיעין-מכבים- רעות",
+      "adress":"אדר 1 מודיעין - הגג האדום",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.9107727,
+         "lng":35.0136194
+      },
+      "data":"Adar St 1, Modi'in-Maccabim-Re'ut, Israel"
+   },
+   {  
+      "city":"רמלה",
+      "adress":"שדרות חיים ויצמן 9 רמלה - היכל התרבות אולם למחול",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.9300849,
+         "lng":34.8663688
+      },
+      "data":"Sderot Hayim Weizman 9, Ramla, Israel"
+   },
+   {  
+      "city":"ראש העין",
+      "adress":"מרבד הקסמים 10 ראש העין - מרכז קהילתי",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.0967863,
+         "lng":34.9452167
+      },
+      "data":"Marvad HaKsamim St 10, Rosh Haayin, Israel"
+   },
+   {  
+      "city":"פתח תקווה",
+      "adress":"עקיבא ליברכט 10 פתח תקווה - אולם כנסים",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.0917504,
+         "lng":34.8847627
+      },
+      "data":"Akiva Librecht St 10, Petah Tikva, Israel"
+   },
+   {  
+      "city":"קריית אונו",
+      "adress":"לוס אנג\"לס 2 קריית אונו - סניף המפלגה",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.0635107,
+         "lng":34.8574914
+      },
+      "data":"Los Angeles St 2, Kiryat Ono, Israel"
+   },
+   {  
+      "city":"גבעתיים",
+      "adress":"משמר הירדן 18 גבעתיים",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.061689,
+         "lng":34.814515
+      },
+      "data":"Mishmar HaYarden St 18, Giv'atayim, Israel"
+   },
+   {  
+      "city":"רמת גן",
+      "adress":"ביאליק 42 רמת גן - בית האזרח",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.0832581,
+         "lng":34.8146179
+      },
+      "data":"Bialik St 42, Ramat Gan, Israel"
+   },
+   {  
+      "city":"בת ים",
+      "adress":"רהב 7 בת ים - החברה לתרבות פנאי וספורט",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.021992,
+         "lng":34.7580198
+      },
+      "data":"Rahav St 7, Bat Yam, Israel"
+   },
+   {  
+      "city":"חולון",
+      "adress":"ארלוזרוב 84 חולון",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.0177188,
+         "lng":34.7721153
+      },
+      "data":"Arlozorov St 84, Holon, Israel"
+   },
+   {  
+      "city":"ראשון לציון",
+      "adress":"רוטשילד 62 ראשון לציון ",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.96417069999999,
+         "lng":34.7988526
+      },
+      "data":"Rothschild St 62, Rishon LeTsiyon, Israel"
+   },
+   {  
+      "city":"יבנה",
+      "adress":"המיסב 1 יבנה - מתחם תחנת דלק אלון ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":31.8641877,
+         "lng":34.7420431
+      },
+      "data":"Ha-Meisav St 1, Yavne, Israel"
+   },
+   {  
+      "city":"רחובות",
+      "adress":"לוין אפשטיין 22 רחובות - אולם מופת",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.8972612,
+         "lng":34.808766
+      },
+      "data":"Levin Epstein St 22, Rehovot, Israel"
+   },
+   {  
+      "city":"נס ציונה",
+      "adress":"דוד אלעזר 22 נס ציונה - מתנ\"ס",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.9291924,
+         "lng":34.8115087
+      },
+      "data":"David Elazar St 22, Ness Ziona, Israel"
+   },
+   {  
+      "city":"נצר סרני",
+      "adress":"נצר סירני - חדר אסיפות ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":31.922789,
+         "lng":34.821191
+      },
+      "data":"Netzer Sereni, Israel"
+   },
+   {  
+      "city":"נען",
+      "adress":"נען - מועדון החברים ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":31.883208,
+         "lng":34.859237
+      },
+      "data":"Na'an, Israel"
+   },
+   {  
+      "city":"אשקלון",
+      "adress":"הפלמ\"ח 3 אשקלון",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.6634889,
+         "lng":34.5859913
+      },
+      "data":"Ha-Palmakh St 3, Ashkelon, Israel"
+   },
+   {  
+      "city":"אשדוד",
+      "adress":"יאיר 4 אשדוד - בית ויצ\"ו",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.8125716,
+         "lng":34.6459206
+      },
+      "data":"Yair St 4, Ashdod, Israel"
+   },
+   {  
+      "city":"שדרות",
+      "adress":"ככר הנשיא 8 שדרות - מרכז צהלולים",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.5253641,
+         "lng":34.59315369999999
+      },
+      "data":"משעול הנשיא 8, שדרות, 8712808, Israel"
+   },
+   {  
+      "city":"בארי",
+      "adress":"בארי - מועדון לחבר",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":31.424513,
+         "lng":34.492567
+      },
+      "data":"Be'eri, Israel"
+   },
+   {  
+      "city":"קריית מלאכי",
+      "adress":"שדרות דוד בן גוריון 51 קריית מלאכי - בית החייל ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":31.7297931,
+         "lng":34.7425676
+      },
+      "data":"Sderot David Ben Gurion 51, Kiryat Malakhi, Israel"
+   },
+   {  
+      "city":"דימונה",
+      "adress":"מ\"ד המעפילים 2 דימונה - היכל התרבות",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.0641574,
+         "lng":35.0341799
+      },
+      "data":"Memorial Dalet HaMa'apilim St 2, Dimona, Israel"
+   },
+   {  
+      "city":"קריית גת",
+      "adress":"שדרת פ\"ז 92 קריית גת",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.6071166,
+         "lng":34.7721347
+      },
+      "data":"Sderat Paz 92, Kiryat Gat, Israel"
+   },
+   {  
+      "city":"באר שבע",
+      "adress":"דרך המשחררים 7 באר שבע - מכללת אתגר",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":31.2510234,
+         "lng":34.795859
+      },
+      "data":"Derech HaMeshahrerim 7, Beersheba, Israel"
+   },
+   {  
+      "city":"ערד",
+      "adress":"אלעזר בן יאיר 28 ערד - מתנ\"ס ערד כיתת אלא ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":31.2582979,
+         "lng":35.2096304
+      },
+      "data":"Elazar Ben Ya'ir St 28, Arad, Israel"
+   },
+   {  
+      "city":"נתיבות",
+      "adress":"הגפן 6 , נתיבות ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":31.415,
+         "lng":34.585626
+      },
+      "data":"HaGefen St 6, Netivot, Israel"
+   },
+   {  
+      "city":"רביבים",
+      "adress":"קיבוץ רביבים מועדון לחבר ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":31.045195,
+         "lng":34.72108600000001
+      },
+      "data":"Revivim, Israel"
+   },
+   {  
+      "city":"אופקים",
+      "adress":"השקד 6 אופקים - מרכז תרבות ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":31.3082678,
+         "lng":34.61868
+      },
+      "data":"HaShaked St 6, Ofakim, Israel"
+   },
+   {  
+      "city":"מצפה רמון",
+      "adress":"עין סהרונים 1 מצפה רמון",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":30.6093863,
+         "lng":34.797118
+      },
+      "data":"Ein Saharonim St 1, Mitzpe Ramon, Israel"
+   },
+   {  
+      "city":"יטבתה",
+      "adress":"יטבתה ח ישיבות מזכירות ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":29.896322,
+         "lng":35.059227
+      },
+      "data":"Yotvata, Israel"
+   },
+   {  
+      "city":"אילת",
+      "adress":"שדרות התמרים 3 אילת - מלון סי הוטל ",
+      "hours":"17:00-21:00",
+      "location":{  
+         "lat":29.5552591,
+         "lng":34.9543661
+      },
+      "data":"Sderot HaTmarim 3, Eilat, Israel"
+   },
+   {  
+      "city":"באקה אל-גרביה",
+      "adress":"מרכז יום לקשיש ליד בנין המתנ\"ס",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.422891,
+         "lng":35.035764
+      },
+      "data":"Baqa al-Gharbiyye, Israel"
+   },
+   {  
+      "city":"יפיע",
+      "adress":"רחוב ראשי יפיע 16955 ליד סלון עמל",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.695002,
+         "lng":35.28201
+      },
+      "data":"Yafa an-Naseriyye, Israel"
+   },
+   {  
+      "city":"שפרעם",
+      "adress":"צומת שפרעם דרום מתחם סונול, שפרעם - אולמי עואדיה",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.80576500000001,
+         "lng":35.169971
+      },
+      "data":"Shefa-'Amr, Israel"
+   },
+   {  
+      "city":"קלנסווה",
+      "adress":"קלנסוואה מתנ\"ס / מרכז קהילתי",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.285791,
+         "lng":34.981487
+      },
+      "data":"Qalansawe, Israel"
+   },
+   {  
+      "city":"טירה",
+      "adress":"אשכול פיס אבובאקר - מאחורי איצטדיון הכדורגל",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.235466,
+         "lng":34.950506
+      },
+      "data":"Tira, Israel"
+   },
+   {  
+      "city":"רהט",
+      "adress":"שכונה 9 1 רהט",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":31.401704,
+         "lng":34.753386
+      },
+      "data":"Rahat, Israel"
+   },
+   {  
+      "city":"בית ג`ן",
+      "adress":"בניין דלתא לשעבר ליד בית העם בית ג`ן",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.965655,
+         "lng":35.381268
+      },
+      "data":"Beit Jann, Israel"
+   },
+   {  
+      "city":"ג`וליס",
+      "adress":"ג`וליס - בית העם ",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":32.9443425,
+         "lng":35.1857616
+      },
+      "data":"Julis, Israel"
+   },
+   {  
+      "city":"מגאר",
+      "adress":"מתנס העמותה לפעילות קהילתית מגאר",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.890704,
+         "lng":35.40461
+      },
+      "data":"Maghar, Israel"
+   },
+   {  
+      "city":"דאלית אל-כרמל",
+      "adress":"כביש ראשי, דאלית אל-כרמל - מכללת הכרמל",
+      "hours":"11:00-21:00",
+      "location":{  
+         "lat":32.693796,
+         "lng":35.055447
+      },
+      "data":"Daliyat al-Karmel, Israel"
+   },
+   {  
+      "city":"חורה",
+      "adress":"מתנ\"ס חורה",
+      "hours":"11:00-15:00",
+      "location":{  
+         "lat":31.2988257,
+         "lng":34.9399148
+      },
+      "data":"Hura, Israel"
+   }
+]
